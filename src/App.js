dfb9e3daef53cc0ba2945e3749f985041d90ce32
Parshuram Reddy <parshuram.sudda@gmail.com>
2023-02-01 16:10:33 -0500

    Revert "Basic App Loading with IFRAME"
    This reverts commit 94156531a9bdf22db1c4c5c8b8f333bc1a7f1ca9.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import Index from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Index />
+      <h1>KeTT APP Loading</h1>
     </div>
   );
 }
